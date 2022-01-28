d5f170a367a814a6bbfdba2afc3a207e46012710
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-01-27 22:10:06 -0600

    3_3

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-const url ="https://platzi-avo.vercel.app/api/avo";
-window.fetch(url)
+const baseUrl = "https://platzi-avo.vercel.app/api/avo";
+const appNode = document.querySelector('#app');
+
+window
+.fetch('${baseUrl}/api/avo')
 .then((respuesta) => respuesta.json())
 .then((responseJson) =>{
     const todosLosItems = [];
     responseJson.data.forEach((item) =>  {
     const imagen = document.createElement('img');
+    imagen.src = item.imagen;
     const title = document.createElement("h2");
+    name.textContent = item.name;
     const price = document.createElement("div");
-    
+    price.textContent = item.price;
+
     const container = document.createElement("div");
     container.append(imagen, title, price);
     todosLosItems.push(container);
 });
-document.body.append(...todosLosItems);
+appNode.append(...todosLosItems);
 });
 
